30bcf1362d45afd6c3ba836e5e81e6d9b25ca2dd
Danny Berger <dannyb@bh.org.il>
2015-01-26 17:54:28 +0200

    improve sub-header border arrow position, resolve BHS-3

--- js/modules/main/src/directives/itemScrollDirective.js
+++ js/modules/main/src/directives/itemScrollDirective.js
@@ -1,9 +1,9 @@
 angular.module('main').
-	directive('itemScroll', [function() {
+	directive('itemScroll', ['$window', function($window) {
 		return {
 			restrict: 'A',
 			scope: {},
-			controller: ['$scope', 'header', function($scope, header) {
+			controller: ['$scope', '$window', 'header', function($scope, $window, header) {
 				Object.defineProperty($scope, 'sub_header_state', {
 					get: function() {
 						return header.sub_header_state;
@@ -14,14 +14,13 @@ angular.module('main').
 				});
 			}],
 			link: function(scope, element) {
-
 				scope.$watch('sub_header_state', function(newVal, oldVal) {
 					if (newVal != 'closed') {
 						element[0].scrollTop = 0;
 					}
 				});
 
-				element.bind('scroll', function() {
+				angular.element($window).bind('scroll', function() {
 					if (element[0].scrollTop != 0 && scope.sub_header_state != 'closed') {
 						scope.$apply(function(scope) {
 							scope.sub_header_state = 'closed';
