6115964007dbec3b4b1b6ce040a3b56ee289a922
Danny Berger <dannyb@bh.org.il>
2015-04-05 16:31:15 +0300

    add recently-viewed scroll

--- js/modules/main/src/controllers/recentlyViewedController.js
+++ js/modules/main/src/controllers/recentlyViewedController.js
@@ -1,4 +1,5 @@
 var RecentlyViewedController = function($state, recentlyViewed, itemTypeMap) {
+	var max_items_inscroll = 14;
 
 	this.$state = $state;
 	this.itemTypeMap = itemTypeMap;
@@ -8,6 +9,16 @@ var RecentlyViewedController = function($state, recentlyViewed, itemTypeMap) {
 			return recentlyViewed.items;
 		}
 	});
+
+    Object.defineProperty(this, 'scroll_offset', {
+    	get: function() {
+    		return this.view_index * -68;
+    	}
+    });
+
+    this.view_index = this.items.length > max_items_inscroll ? this.items.length - max_items_inscroll : 0;
+
+    window.recentlyViewedCtrl = this;
 };	
 
 RecentlyViewedController.prototype = {
@@ -15,6 +26,14 @@ RecentlyViewedController.prototype = {
     	var collection_name = this.itemTypeMap.get_collection_name(item_data);;
     	var item_string = collection_name + '.' + item_data._id; 
         this.$state.go('item-view', {item_string: item_string});
+    },
+
+    scroll_left: function() {
+    	this.view_index++;
+    },
+
+    scroll_right: function() {
+    	this.view_index--;
     }
 };
 
