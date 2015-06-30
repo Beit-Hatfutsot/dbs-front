2fe0a9a9a3856a660162ab3cca3b400b24a20945
Danny Berger <dannyb@bh.org.il>
2015-06-30 16:45:46 +0300

    swith to new production API

--- js/modules/api_client/src/services/apiClientService.js
+++ js/modules/api_client/src/services/apiClientService.js
@@ -2,29 +2,29 @@ angular.module('apiClient').
 	factory('apiClient', function() {
 	
 	  	return {
-	  		/*
+	  		
+	  		// urls: {
+	  		// 	auth: 			'http://127.0.0.1:5000/auth',
+	  		// 	user: 			'http://127.0.0.1:5000/user', 
+	  		// 	mjs: 			'http://127.0.0.1:5000/mjs', 
+	  		// 	wizard_search: 	'http://127.0.0.1:5000/wsearch',
+	  		// 	item: 			'http://127.0.0.1:5000/item',
+	  		// 	suggest: 		'http://127.0.0.1:5000/suggest',
+	  		// 	ftrees_search:	'http://127.0.0.1:5000/fsearch',
+	  		// 	ftrees_get: 	'http://127.0.0.1:5000/get_ftree_url',
+	  		// 	upload: 		'http://127.0.0.1:5000/upload'
+	  		// }
+	  		
 	  		urls: {
-	  			auth: 			'http://127.0.0.1:5000/auth',
-	  			user: 			'http://127.0.0.1:5000/user', 
-	  			mjs: 			'http://127.0.0.1:5000/mjs', 
-	  			wizard_search: 	'http://127.0.0.1:5000/wsearch',
-	  			item: 			'http://127.0.0.1:5000/item',
-	  			suggest: 		'http://127.0.0.1:5000/suggest',
-	  			ftrees_search:	'http://127.0.0.1:5000/fsearch',
-	  			ftrees_get: 	'http://127.0.0.1:5000/get_ftree_url',
-	  			upload: 		'http://127.0.0.1:5000/upload'
-	  		}
-	  		*/
-	  		urls: {
-	  			auth: 			'http://bhsapi.ezdr.net/auth',
-	  			user: 			'http://bhsapi.ezdr.net/user', 
-	  			mjs: 			'http://bhsapi.ezdr.net/mjs', 
-	  			wizard_search: 	'http://bhsapi.ezdr.net/wsearch',
-	  			item: 			'http://bhsapi.ezdr.net/item',
-	  			suggest: 		'http://bhsapi.ezdr.net/suggest',
-	  			ftrees_search:	'http://bhsapi.ezdr.net/fsearch',
-	  			ftrees_get: 	'http://bhsapi.ezdr.net/get_ftree_url',
-	  			upload: 		'http://bhsapi.ezdr.net/upload'
+	  			auth: 			'http://api.myjewishidentity.org/auth',
+	  			user: 			'http://api.myjewishidentity.org/user', 
+	  			mjs: 			'http://api.myjewishidentity.org/mjs', 
+	  			wizard_search: 	'http://api.myjewishidentity.org/wsearch',
+	  			item: 			'http://api.myjewishidentity.org/item',
+	  			suggest: 		'http://api.myjewishidentity.org/suggest',
+	  			ftrees_search:	'http://api.myjewishidentity.org/fsearch',
+	  			ftrees_get: 	'http://api.myjewishidentity.org/get_ftree_url',
+	  			upload: 		'http://api.myjewishidentity.org/upload'
 	  		}
 	  	};
 	});
