46d67f3dd75437fad216693d1ea5236090514490
Danny Berger <dannyb@bh.org.il>
2015-07-12 11:06:25 +0300

    actually use preventClick directive

--- js/modules/main/src/directives/preventRightclickDirective.js
+++ js/modules/main/src/directives/preventRightclickDirective.js
@@ -2,7 +2,7 @@ angular.module('main').
 	directive('preventRightclick', [function() {
 		return {
 			restrict: 'A',
-			link: function(scope, element, attrs) {
+			link: function(scope, element) {
 				element[0].addEventListener('contextmenu', function(e) {
 					e.preventDefault();
 				}, false);
