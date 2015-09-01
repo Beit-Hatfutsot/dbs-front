56bf7c5424b2ba7e8d8eb3cbdd1ae789facbe276
Benny Daon <bennydaon@gmail.com>
2015-09-01 17:34:54 +0300

    rinsing the code with item-type directive

---
+++ js/modules/main/src/directives/itemTypeDirective.js
@@ -0,0 +1,14 @@
+angular.module('main').directive('itemType', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/main/item-type.html',
+		scope: {
+			type: '=',
+			image: '=',
+		},
+		// controller: 'ModalDialogCtrl as modalDialogController'
+	};
+});
+
+
+
