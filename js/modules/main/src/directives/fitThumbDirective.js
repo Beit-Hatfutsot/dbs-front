281ceb870581c5bc43a45004d444fcb0abd5e65f
inna-r <inna.romanenko90@gmail.com>
2015-12-20 17:06:42 +0200

    adding google search,
    adding middle/right click on item-preview

--- js/modules/main/src/directives/fitThumbDirective.js
+++ js/modules/main/src/directives/fitThumbDirective.js
@@ -3,23 +3,34 @@ angular.module('main').directive('fitThumb', ['$timeout', function($timeout) {
 		restrict: 'A',
 		scope: {},
 		link: function(scope, element) {
-			
-			$timeout(function() {
-				var width = element[0].naturalWidth,
-				height = element[0].naturalHeight,
-				ratio;
-				
-				if (width !== undefined && height !== undefined) {
-					ratio = height/width;
-				
-					if (ratio > 0.65) {
-						element.css('height', '100%');
-					}
-					else {
-						element.css('width', '100%');
-					}
+
+			function check_tries(tries) {
+				if (isNaN(+tries)) {
+					tries = 5;
 				}
-			});
+				if(tries > 0){
+					$timeout(function() {
+						var width = element[0].naturalWidth,
+						height = element[0].naturalHeight,
+						ratio;
+						if (width) {
+							ratio = height/width;
+						
+							if (ratio > 0.65) {
+								element.css('height', '100%');
+							}
+							else {
+								element.css('width', '100%');
+							}
+						} else {
+							check_tries(tries - 1);
+						}
+					}, 100);
+				}
+			}
+			check_tries(10);
 		}
 	}
 }]);
+
+
