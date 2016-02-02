90e469a1e5daa5d68324576defeb8b896dfc1527
Benny Daon <bennydaon@gmail.com>
2016-02-02 09:25:09 +0200

    Remove the wizard-result old code

--- js/modules/main/src/directives/wizardResultDirective.js
+++
@@ -1,9 +0,0 @@
-angular.module('main').
-	directive('wizardResult', [function() {
-		return {
-			restrict: 'E',
-			replace: true,
-			templateUrl: 'templates/main/wizard-result.html',
-			controller: 'WizardResultCtrl as wizardResultController'
-		};
-	}]);
