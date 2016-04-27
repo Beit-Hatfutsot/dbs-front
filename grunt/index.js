542f1943a426dcc0af523e03447b971235f6da75
alonisser <alonisser@gmail.com>
2016-04-28 00:18:07 +0300

    Working configuration creating a revved minified dist

--- grunt/index.js
+++ grunt/index.js
@@ -1,9 +1,10 @@
 var path = require('path');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     var public_dir = 'public/';
-    var pkg =  grunt.file.readJSON('package.json')
+    var dist_dir = 'dist/';
+    var pkg = grunt.file.readJSON('package.json');
 
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
@@ -12,14 +13,27 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
 
     var config = require('load-grunt-config')(grunt, {
-        init:true,
-        configPath: path.join(process.cwd(), 'grunt','options'),
-        data:{}
+        init: true,
+        configPath: path.join(process.cwd(), 'grunt', 'options'),
+        data: {}
     });
-    grunt.config.set('public_dir',public_dir);
-    grunt.config.set('pkg',pkg);
-    
+    grunt.config.set('public_dir', public_dir);
+    grunt.config.set('dist_dir', dist_dir);
+    grunt.config.set('pkg', pkg);
+
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('build', ['clean:public', 'sass', 'concat', 'uglify', 'copy']);
+    grunt.registerTask('build', ['clean:public',
+        'clean:dist',
+        'sass:dist',
+        'copy:public',
+        'copy:dist',
+        'useminPrepare',
+
+        'concat',
+        'cssmin',
+        'uglify',
+
+         'rev',
+        'usemin']);
 };
 
--- grunt/options/clean.js
+++ grunt/options/clean.js
@@ -1,4 +1,5 @@
 module.exports = {
     public: ['<%= public_dir %>'],
+    dist: ['<%= dist_dir %>'],
     bower: ['bower_components/']
 };
