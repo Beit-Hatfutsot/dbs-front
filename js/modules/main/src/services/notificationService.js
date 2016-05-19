1642dd993c9ee59ff2fe8499f734d240990da290
Benny Daon <bennydaon@gmail.com>
2016-05-19 16:00:11 +0300

    English!!!

--- js/modules/main/src/services/notificationService.js
+++ js/modules/main/src/services/notificationService.js
@@ -26,7 +26,7 @@ angular.module('main').
 	he: 'הכל אחלה',
 	options: pendingOptions
 }, 1: {
-	en: "Our pigeon is on her way, carrying your login link",
+	en: "Our pigeon is on its way, carrying your login link",
 	he: 'נשלחה הודעת דואל עם קישור לכניסה לחשבונך'
 }, 2: {
 	en: 'found some people in the family trees',
