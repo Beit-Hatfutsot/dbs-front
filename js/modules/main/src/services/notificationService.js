bfedb108002bffcc937715ac1a5df31ce89becbe
inna-r <inna.romanenko90@gmail.com>
2016-07-04 13:22:07 +0300

    autohide wizard search error message
    issue #134

--- js/modules/main/src/services/notificationService.js
+++ js/modules/main/src/services/notificationService.js
@@ -17,95 +17,94 @@ angular.module('main').
 			 className: 'warn'
 		},
 		messages = {
-500: {
-	en: 'Sorry, but the Server is down. Please try again later',
-	he: 'צטערת, השרת נפל. אנא נסה שנית מאוחר יותר.',
-	options: errorOptions
-}, 0: {
-	en: 'all is fine',
-	he: 'הכל אחלה',
-	options: pendingOptions
-}, 1: {
-	en: "Our pigeon is on its way, carrying your login link",
-	he: 'נשלחה הודעת דואל עם קישור לכניסה לחשבונך'
-}, 2: {
-	en: 'found some people in the family trees',
-	he: 'מצטתי אנשים בעצי המשפחהן',
-}, 3: {
-	en: "Sorry, didn't find any person",
-	he: 'מצטערים, לא מצאתי אנשים',
-	options: errorOptions
-}, 4: {
-	en: 'Loading item...',
-	he: 'טוען פריט...',
-	options: pendingOptions
-}, 5: {
-	en: 'Sorry, failed to fetch item',
-	he: 'מצטערים, טעינת פריט נכשלה'
-}, 7: {
-	en: 'Item removed',
-	he: 'הפריט הוסר'
-}, 8: {
-	en: 'Failed to remove item',
-	he: 'הסרת הפריט נכשלה'
-}, 9: {
-	en: 'Upload in progress...',
-	he: 'העלאה מתבצעת...'
-}, 10: {
-	en: 'Sending the login email has failed, please try again later.',
-	he: 'שליחת דואל הכניסה נשלחה, אנא נסו שוב בעוד מספר רגעים',
-	options: errorOptions
-}, 11: {
-	en: 'Search has failed.',
-	he: 'החיפוש נכשל.',
-	options: errorOptions
-}, 12: {
-	en: 'We have not found a surname or a community to match your search',
-	he: 'לא מצאנו את שם המשפחה או הקהילה שחיפשתם',
-	options: {
-	  className: 'error',
-	  autoHide: false,
-	  showDuration: 10
-	  }
-}, 13: {
-	en: 'Sorry, we failed to find a community to match your search',
-	he: 'לא מצאנו את הקהילה שחיפשתם',
-	options: warnOptions
-}, 14: {
-	en: 'Sorry, we failed to find a family name to match your search',
-	he: 'לא מצאנו את שם המפשחה שחיפשתם',
-	options: warnOptions
+      500: {
+      	en: 'Sorry, but the Server is down. Please try again later',
+      	he: 'צטערת, השרת נפל. אנא נסה שנית מאוחר יותר.',
+      	options: errorOptions
+      }, 0: {
+      	en: 'all is fine',
+      	he: 'הכל אחלה',
+      	options: pendingOptions
+      }, 1: {
+      	en: "Our pigeon is on its way, carrying your login link",
+      	he: 'נשלחה הודעת דואל עם קישור לכניסה לחשבונך'
+      }, 2: {
+      	en: 'found some people in the family trees',
+      	he: 'מצטתי אנשים בעצי המשפחהן',
+      }, 3: {
+      	en: "Sorry, didn't find any person",
+      	he: 'מצטערים, לא מצאתי אנשים',
+      	options: errorOptions
+      }, 4: {
+      	en: 'Loading item...',
+      	he: 'טוען פריט...',
+      	options: pendingOptions
+      }, 5: {
+      	en: 'Sorry, failed to fetch item',
+      	he: 'מצטערים, טעינת פריט נכשלה'
+      }, 7: {
+      	en: 'Item removed',
+      	he: 'הפריט הוסר'
+      }, 8: {
+      	en: 'Failed to remove item',
+      	he: 'הסרת הפריט נכשלה'
+      }, 9: {
+      	en: 'Upload in progress...',
+      	he: 'העלאה מתבצעת...'
+      }, 10: {
+      	en: 'Sending the login email has failed, please try again later.',
+      	he: 'שליחת דואל הכניסה נשלחה, אנא נסו שוב בעוד מספר רגעים',
+      	options: errorOptions
+      }, 11: {
+      	en: 'Search has failed.',
+      	he: 'החיפוש נכשל.',
+      	options: errorOptions
+      }, 12: {
+      	en: 'We have not found a surname or a community to match your search',
+      	he: 'לא מצאנו את שם המשפחה או הקהילה שחיפשתם',
+      	options: {
+      	  className: 'error',
+      	  showDuration: 10
+      	  }
+      }, 13: {
+      	en: 'Sorry, we failed to find a community to match your search',
+      	he: 'לא מצאנו את הקהילה שחיפשתם',
+      	options: warnOptions
+      }, 14: {
+      	en: 'Sorry, we failed to find a family name to match your search',
+      	he: 'לא מצאנו את שם המפשחה שחיפשתם',
+      	options: warnOptions
 
-}, 15: {
-	en: 'Fetching more items...',
-	he: 'אוסף עוד פריטים...',
-	options: pendingOptions
-	
-}, 16: {
-	en: 'You must complete renaming before navigating',
-	he: 'נא להשלים את שינוי השם',
-	options: errorOptions
-}, 17: {
-	en: 'Sorry, this login link is not valid anymore',
-	he: 'מצטערים, הקישור אינו תקין',
-	options: errorOptions
-}, 18: {
-	en: 'This file type is not supported. Please upload a JPG or PNG file.',
-	he: 'שוג הקובץ הזה אינו נתמך, אנא נסו להעלות קובץ מסוג נתמך',
-	options: errorOptions
-}, 19: {
-	en: 'Something went wrong, please try uploading again',
-	he: 'משהו השתבש, אנא נסו שוב',
-	options: errorOptions
-}
+      }, 15: {
+      	en: 'Fetching more items...',
+      	he: 'אוסף עוד פריטים...',
+      	options: pendingOptions
+
+      }, 16: {
+      	en: 'You must complete renaming before navigating',
+      	he: 'נא להשלים את שינוי השם',
+      	options: errorOptions
+      }, 17: {
+      	en: 'Sorry, this login link is not valid anymore',
+      	he: 'מצטערים, הקישור אינו תקין',
+      	options: errorOptions
+      }, 18: {
+      	en: 'This file type is not supported. Please upload a JPG or PNG file.',
+      	he: 'שוג הקובץ הזה אינו נתמך, אנא נסו להעלות קובץ מסוג נתמך',
+      	options: errorOptions
+      }, 19: {
+      	en: 'Something went wrong, please try uploading again',
+      	he: 'משהו השתבש, אנא נסו שוב',
+      	options: errorOptions
+      }
 
 
 // next line ends the notifications dict
-	},
-	message = {
-			en: '',
-			he: ''
-		}
+	  },
+  	message = {
+  			en: '',
+  			he: ''
+  	}
 
 		var notification = {
 			loading_gif: null,
