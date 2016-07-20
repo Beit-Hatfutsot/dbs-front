de2491a5caa3bfe6d36fc97a60737c1923968d5e
Benny Daon <bennydaon@gmail.com>
2016-07-20 10:29:05 +0300

    fixing the share directive testsi - #127

--- js/modules/social/test/directives/shareDirectiveSpec.js
+++ js/modules/social/test/directives/shareDirectiveSpec.js
@@ -4,36 +4,42 @@ describe('share-directives', function() {
 
 	var $scope, $compile;
 
-	module('social');
-	module('bhsclient-templates');
+	beforeEach(function() {
+		module('social');
+		module('bhsclient-templates');
+	});
+	beforeEach(inject(function($injector) {
+	    var $httpBackend = $injector.get('$httpBackend');
+	    // $httpBackend.whenGET('templates/social/share.html').passThrough();
+	}));
 	beforeEach(inject(function (_$compile_, $rootScope) {
 	    $compile = _$compile_;
 	    $scope = $rootScope.$new();
 	}));
 
-	it('should use the href provided',
+	it('should create a safe_href',
 	 function() {
-		var template = $compile("<share href='http://example.com' link />")($scope);
+		var template = $compile("<share href='link' link />")($scope);
 
-		$scope.$digest()
-		expect (template[0].outerHTML)
-			.toMatch(/<a[^>]+http:\/\/example\.com/);
+		$scope.link="http://example.com";
+		$scope.$digest();
+		expect(template.isolateScope().safe_href).toMatch("http%3A%2F%2Fexample.com");
 	});
 
 	it('should generate facebook link',
 	 function() {
-		var template = $compile("<share href='http://example.com' facebook />")($scope);
+		var template = $compile("<share href='\"http://example.com\"' facebook />")($scope);
 
-		$scope.$digest()
+		$scope.$digest();
 		expect (template[0].outerHTML)
 			.toMatch(/<a[^>]+https:\/\/www.facebook.com\/dialog\/share/);
 	});
 
 	it('should generate twitter link',
 	 function() {
-		var template = $compile("<share href='http://example.com' twitter />")($scope);
+		var template = $compile("<share href='\"http://example.com\"' twitter />")($scope);
 
-		$scope.$digest()
+		$scope.$digest();
 		expect (template[0].outerHTML)
 			.toMatch(/<a[^>]+https:\/\/twitter.com\/intent\/tweet/);
 	});
