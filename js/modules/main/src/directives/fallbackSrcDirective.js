4bdbc84b153b3aaa072b2321a3534cc0f5cd1295
inna-r <inna.romanenko90@gmail.com>
2017-06-01 10:40:38 +0300

    add link to fallback picture and cancel open gallery
    issue #405

--- js/modules/main/src/directives/fallbackSrcDirective.js
+++ js/modules/main/src/directives/fallbackSrcDirective.js
@@ -4,7 +4,14 @@ angular.module('main').
         link: function(scope, element, attrs) {
           element.bind('error', function() {
             if (attrs.src != attrs.fallbackSrc) {
+            //if the picture path returns error, directive sets fallback source
               attrs.$set('src', attrs.fallbackSrc);
+              var path = (scope.lang=='en')?'http://www.bh.org.il/about-us/volunteering/':
+                          'http://www.bh.org.il/he/%D7%90%D7%95%D7%93%D7%95%D7%AA%D7%99%D7%A0%D7%95/%D7%94%D7%AA%D7%A0%D7%93%D7%91%D7%95%D7%AA-2/';
+              //add link
+              element[0].parentElement.setAttribute('href', path);
+              //cancel open gallery
+              element.off('click');
             }
           });
 
